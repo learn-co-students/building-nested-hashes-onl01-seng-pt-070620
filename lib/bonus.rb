6d78365430b8eee1948dec57511348c10d54070e
Eugene White <eugenewhite1994@gmail.com>
2020-07-18 21:38:19 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,11 +1,15 @@
-
-
-  def bonus
+def bonus
+ epic_tragedy = {
+   :montague => {
+      :patriarch => {name: "Michael Jordan", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero_friends => [
          {name: "Benvolio", age: "17", attitude: "worried"},
          {name: "Steven", age: "30", attitude: "confused"}
       ]
    }, 
-   },
+   
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
